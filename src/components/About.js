efad669fae611ecaf969ac5dae07d19d249a71d2
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-29 13:08:31 -0800

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <img src='/images/react-icon.png' height='128px' width='128px'></img>
+      <img src='/images/ruby-on-rails-icon.png' height='128px' width='128px'></img>
+      <img src='/images/postgresql-icon.png' height='128px' width='128px'></img>
+    </div>
+  )
+}
---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div>
+      <h2>I'm a software engineer fluent in Hungarian and ready to innovate on the international scene.
+      I like clean UIs and performant back-ends.</h2>
+    </div>
+  )
+}
