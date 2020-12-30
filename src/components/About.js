e3b9fe833400b4a40115c3bc8520a6fb865b49bd
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-30 11:42:37 -0800

    add alt prop

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function About() {
   return (
     <div className='image-container'>
-      <img className='techstack-image' src='/images/react-icon.png' height='128px' width='128px'></img>
-      <img className='techstack-image' src='/images/ruby-on-rails-icon.png' height='128px' width='128px'></img>
-      <img className='techstack-image' src='/images/postgresql-icon.png' height='128px' width='128px'></img>
+      <img className='techstack-image' src='/images/react-icon.png' height='128px' width='128px' alt='react'></img>
+      <img className='techstack-image' src='/images/ruby-on-rails-icon.png' height='128px' width='128px' alt='ruby on rails'></img>
+      <img className='techstack-image' src='/images/postgresql-icon.png' height='128px' width='128px' alt='postgresql'></img>
     </div>
   )
 }
