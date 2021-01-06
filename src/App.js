ac530008e4717de9c8efe44e41c37550d7058ea3
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-01-06 11:33:35 -0800

    add about me header and fix padding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='App'>
       <h1>Samuel Grasse-Haroldsen</h1>
-      <h2>in an instance</h2>
+      <h2 className='description'>in an instance</h2>
       <div className='image-container'>
         <img className='image' src='images/mini_sam.png' alt='Me'></img>
         <Terminal />
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Intro() {
   return (
     <div>
+      <h2>About Me</h2>
       <p className='text'>I'm a software engineer fluent in Hungarian and ready to innovate on the international scene.
       I like clean UIs and performant back-ends.</p>
     </div>
